3f6fc8ac04c353b1aef7f9b1dbaddf6d34ffa805
Tim O'Farrell <tofarr@gmail.com>
2025-09-08 15:52:33 -0600

    Import fix

--- eventy/fs/abstract_file_event_queue.py
+++ eventy/fs/abstract_file_event_queue.py
@@ -12,8 +12,8 @@ from eventy.event_result import EventResult
 from eventy.page import Page
 from eventy.queue_event import QueueEvent
 from eventy.serializers.serializer import Serializer, get_default_serializer
-from eventy.subscriber.subscriber import Subscriber
-from eventy.subscriber.subscription import Subscription
+from eventy.subscriber import Subscriber
+from eventy.subscription import Subscription
 
 T = TypeVar("T")
 _LOGGER = logging.getLogger(__name__)
